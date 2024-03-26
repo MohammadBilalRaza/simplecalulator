4313a818205c43e77b162e2ee79e679f2df57a50
Mohammad Bilal Raza <mohammadbilalraza15@gmail.com>
2024-03-26 14:33:39 +0500

    simplecalculator

---
+++ class.ts
@@ -0,0 +1,31 @@
+// TOPIC FUNCTION
+
+// function square(){
+// let x = 5
+// console.log(x*x)
+// }
+// square()  = jab bh pure program mai square krna hoga a yai likhna ha auto ho jae ga
+
+// function square(num){ //num jo likha isko : parametre receive krty ha
+//     console.log(num * num);
+// }
+// square(10) // 10 : argument pass krty ha
+// square(5)
+// square(25)
+
+// asignment
+// function Areaofcircle(radius){
+// const pi= 3.142
+// console.log(pi* radius^2)
+// }
+// Areaofcircle(5)
+
+// example
+
+// function cook(dish:string){
+//     console.log("making" + ' is in process')
+//     console.log(dish + ' is ready');
+//     return dish + 'khalo'
+// }
+// let cookedFood = cook('biryani ');
+// console.log(cookedFood)
